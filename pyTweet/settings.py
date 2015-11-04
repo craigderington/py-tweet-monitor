7ca339b19f9c9f596fcc1e7b782725d2013c6a76
Craig Derington <cderington@gmail.com>
2015-11-04 13:37:45 -0500

    configure tweepy and Twiiter OAuth

--- pyTweet/settings.py
+++ pyTweet/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'PyTweetMonitor',
     'crispy_forms',
     'bootstrap3',
     'rest_framework',
--- pyTweetMonitor/views.py
+++ pyTweetMonitor/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is the PyTweetMonitor index page')
-    # return render(request, 'pyTweetMonitor/index.html', context)
+
+    # read our json file line by line
+    tweets = {}
+    with open('data/stream_python.json', 'r') as tweet_data:
+        for line in tweet_data:
+            try:
+                tweet = json.loads(line)
+                for key, value in tweet.items():
+                    if key == 'text':
+                        print(value)
+            except:
+                continue
+
+    return render(request, 'pyTweetMonitor/index.html', tweets)
